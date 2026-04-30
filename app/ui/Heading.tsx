29ab812208d04b5c910760f3d737df3cf071e7b0
Abigail Hyde <33608981+abigailhyde@users.noreply.github.com>
2026-04-30 10:08:40 -1000

    fixes typing issue

--- app/ui/Heading.tsx
+++ app/ui/Heading.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export function Heading({children}: { children: string}) : React.ReactNode {
+export function Heading({children}: { children: React.ReactNode}) : React.ReactNode {
     return (
         <h1 className="w-full text-center text-3xl mb-2 font-medium">{children}</h1>
     )
